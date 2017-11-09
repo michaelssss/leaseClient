d9928e8fdd9a76eb3aea79db12f3d8d34d51fc3f
梁煜铭 <yuming_liang@daydao.com>
2017-11-09 15:53:33 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"leaseServer/domain"
+	"time"
+	"fmt"
+	"os"
+	"net"
+)
+
+func main() {
+	ticket := time.NewTicker(time.Second * 20)
+	func() {
+		for _ = range ticket.C {
+			client := domain.Client{ClientName: "bbbbb", ClientAddr: getAddr().(*net.IPNet).IP.String()}
+			fmt.Println(client)
+			client.MakeDiscover()
+		}
+	}()
+}
+func getAddr() net.Addr {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, address := range addrs {
+
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return address
+			}
+
+		}
+	}
+	return nil
+}
