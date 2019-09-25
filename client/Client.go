bcd5910e98f7de488ab1d9fd86dafcffd813bcab
Michaelssss <liangyuming2@gmail.com>
2019-09-25 20:47:05 +0800

    ipv6

--- client/Client.go
+++ client/Client.go
@@ -1,6 +1,7 @@
 package leaseClient
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -8,9 +9,30 @@ import (
 	"time"
 )
 
+// 自定义Dialer
+var myDial = &net.Dialer{
+	Timeout:   10 * time.Second,
+	KeepAlive: 30 * time.Second,
+	DualStack: false,
+}
+
+// 自定义DialContext
+var myDialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+	//network = "tcp4" //仅使用ipv4
+	network = "tcp6" //仅使用ipv6
+	return myDial.DialContext(ctx, network, addr)
+}
+
 func MakeDiscover() net.IP {
 	ipdiscoverlist := []string{"http://ip.zxinc.org/getip"}
 	http.DefaultClient.Timeout = time.Second * 10
+	http.DefaultClient.Transport = &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		DialContext:           myDialContext,
+		MaxIdleConns:          20,
+		IdleConnTimeout:       20 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
 	var rsp *http.Response
 	var err error
 	for index := 0; index < len(ipdiscoverlist); index++ {
