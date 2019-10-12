124615a6698e6dc09c059f331b0402d21cbb0356
Michaelssss <liangyuming2@gmail.com>
2019-10-12 21:57:57 +0800

    ipv6

--- client/Client.go
+++ client/Client.go
@@ -6,6 +6,9 @@ import (
 
 func MakeDiscover() net.IP {
 	conn, err := net.Dial("tcp6", "ipv6.michaelssss.cc:80")
+	if nil != conn {
+		defer conn.Close()
+	}
 	if nil == err {
 		return conn.LocalAddr().(*net.IPNet).IP
 	}
